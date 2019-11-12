46af7cda8b61b2650959689c8d946ec34a905c41
Yang Li <liyang0525@hotmail.com>
2019-11-11 16:17:48 -0800

    home page JS done

--- JS/home.js
+++ JS/home.js
@@ -1,5 +1,49 @@
 $(document).ready(function () {
 
+    //Create new expense function
+    class Expense {
+        constructor(category, amount, date, note) {
+        this.newCategory = category;
+        this.newAmount = amount;
+        this.newDate = date;
+        this.newNote = note;
+        }
+
+        set category(category) {
+            this.newCategory =category;
+        }
+
+        get category() {
+            return this.newCategory;
+        }
+
+        set amount(amount) {
+            this.newAmount = amount;
+        }
+
+        get amount() {
+            return this.newAmount;
+        }
+
+        set date(date) {
+            this.newDate = date;
+        }
+
+        get date() {
+            return this.newDate;
+        }
+
+        set note(note) {
+            this.newNote = note;
+        }
+
+        get note() {
+            return this.newNote;
+        }
+    }
+    //Create new expense item object
+    let userExpense = new Expense();
+
     //Category slide in animation
     var slideIn = {
         "margin-top": "0px",
@@ -32,7 +76,7 @@ $(document).ready(function () {
         "easing": "swing",
         "opacity": "1"
     };
-    
+
     //Details page slide out animation
     var infoSlideOut = {
         "margin-left": "-100%",
@@ -40,33 +84,125 @@ $(document).ready(function () {
         "opacity": "0"
     };
 
-    //After save jump to home stage animation
-    var saveExpense = {
-        "margin-top": "-100%",
-        "easing": "swing",
-        "opacity": "0"
-    };
-
-    //next button event: open expense fill info page
-    $("#next-btn").click(function () {
+    //Choose category event: open expense fill info page
+    $(".category-card").click(function () {
         $("#enter-slide").animate(infoSlideIn, 1000);
         $("body").css("overflow", "hidden");
     });
 
+    //Depends on which category being choosed
+    $("#food-card").click(function () {
+        userExpense.category = "Food: $";
+    });
+
+    $("#health-card").click(function () {
+        userExpense.category = "Health: $";
+    });
+
+    $("#auto-card").click(function () {
+        userExpense.category = "Autos: $";
+    });
+
+    $("#shopping-card").click(function () {
+        userExpense.category = "Clothes: $";
+    });
+
+    $("#sport-card").click(function () {
+        userExpense.category = "Sports: $";
+    });
+
+    $("#education-card").click(function () {
+        userExpense.category = "Education: $";
+    });
+
+    $("#bill-card").click(function () {
+        userExpense.category = "Bill: $";
+    });
+
+    $("#other-card").click(function () {
+        userExpense.category = "Other: $";
+    });
+    
     //go back to category page
     $("#back-btn").click(function () {
         $("#enter-slide").animate(infoSlideOut, 1000);
     });
 
     //save the new expense: return to home page
-    $("#save-btn").click(function() {
+    var flag = false;
+    
+    var expenseControl = $("#expense_store");
+    var expenseList = [];
+
+    $("#save-btn").click(function () {
         $("#enter-slide").animate(infoSlideOut, 1000);
         $("#slideShow").animate(slideOut, 500);
         $("body").css("overflow", "auto");
+        
+        //Retrive value from the input type
+        userExpense.date = document.getElementById('date-field').value;
+        userExpense.amount = document.getElementById('amount-field').value;
+        userExpense.note = document.getElementById('note-field').value;
+        expenseList.push(userExpense.amount);
+
+        //Create new Time line Block
+        var newTimeLineBlock = $("<div></div>");
+
+        if(!flag) {
+            flag = true;
+            $(newTimeLineBlock).addClass("timeline-block timeline-block-left");
+        } else {
+            flag = false;
+            $(newTimeLineBlock).addClass("timeline-block timeline-block-right");
+        }
+
+        var newMarker = $("<div class=marker></div>");
+        var newTimeLineContent = $("<div class=timeline-content></div>");
+        
+        //Create new Timeline details
+        var newTimeLineTitle = $("<h3 class=category></h3>");
+        newTimeLineTitle.append(userExpense.category, userExpense.amount);
+
+        var newTimeLineDate = $("<span class=date></span>");
+        newTimeLineDate.append(userExpense.date);
+
+        var newTimeLineNote = $("<p class=description></p>");
+        newTimeLineNote.append(userExpense.note);
+
+        newTimeLineContent.append(newTimeLineTitle,newTimeLineDate,newTimeLineNote );
+        newTimeLineBlock.append(newMarker, newTimeLineContent);
+
+        $(".timeline-container").prepend(newTimeLineBlock);
+        
+        //Update expenses
+        var expenseTotal = 0;
+        for (var i in expenseList) {
+            expenseTotal += (1 * expenseList[i]);
+        }
+        //Write the new expenses
+        $(expenseControl).html("$" + (expenseTotal));
+
+        //Update the percentage
+        var updatePercent = (1 - (expenseTotal / budgetStore)) * 100;
+        $(".budget_percent_num").html(updatePercent + "%");
+    });
+    
+    //Open up the budget setup modal
+    $("#home-icon").click(function () {
+        $("#setup-budget").fadeIn();
     });
 
-    $("#home-icon").click(function() {
-        $("html, body").animate({scrollTop: 0}, "fast");
+    //Set the budet number
+    var budgetStore = 0;
+    $("#submit-budget-btn").click(function () {
+        var userInput = document.getElementById('user-budget').value;
+        if (userInput < 0) {
+            alert('wtf');
+        } else {
+            $("#budget_store").html("$" + (userInput / 1));
+            $("#setup-budget").fadeOut();
+            budgetStore = userInput;
+        }
     });
 
 });
