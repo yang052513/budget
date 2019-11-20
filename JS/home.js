cdb22911fc414b223f12af2612410067aa7a8b2d
Yang Li <liyang0525@hotmail.com>
2019-11-19 20:58:22 -0800

    fix all the calculation bugs JS and firebase

--- JS/home.js
+++ JS/home.js
@@ -1,82 +1,100 @@
 $(document).ready(function () {
     var db = firebase.firestore();
+    var budgetStore = 0;
+    var expenseUpdate = 0;
 
-    // if the current user logged in user
-    // is authenticated, then grab "uid" "displayName" and "email"
-    // use "set()" with merge (if document did not exist it will be created)   
-    function createUser() {
-        firebase.auth().onAuthStateChanged(function (user) {
-            db.collection("user")
-                .doc(user.uid)
-                .set({
-                    "name": user.displayName,
-                    "email": user.email,
-                }, {
-                    merge: true
-                });
-
-            //Get user name displayed
-            db.collection("user").doc(user.uid).onSnapshot(function (snap) {
-                document.getElementById("user-name").innerHTML = snap.data().name;
+    //Once the current user being logged in
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("user")
+            .doc(user.uid)
+            .set({
+                "name": user.displayName,
+                "email": user.email,
+            }, {
+                merge: true
             });
 
-            //Get user stored budget
-            db.collection("user").doc(user.uid).onSnapshot(function (snap) {
+        //Get user name displayed
+        db.collection("user").doc(user.uid).onSnapshot(function (snap) {
+            document.getElementById("user-name").innerHTML = snap.data().name;
+        });
+
+        //Collect user data based on their ID, Read data from firebase
+        db.collection("user").doc(user.uid).onSnapshot(function (snap) {
+            //When budget > 0 which has previous data, write old data
+            if (snap.data().BudgetStore > 0) {
                 document.getElementById("budget_store").innerHTML = "$" + snap.data().BudgetStore;
+                budgetStore = snap.data().BudgetStore;
+            } else {
+                document.getElementById("budget_store").innerHTML = "$" + 0;
+            }
+
+            //When expense > 0, read previous data
+            if (snap.data().ExpenseStore > 0) {
                 document.getElementById("expense_store").innerHTML = "$" + snap.data().ExpenseStore;
+                expenseUpdate = snap.data().ExpenseStore;
+                console.log(expenseUpdate);
+            } else {
+                document.getElementById("expense_store").innerHTML = "$" + 0;
+            }
+
+            //When Percent > 0, read previous data
+            if (snap.data().PercentStore > 0) {
                 $(".budget_percent_num").html(snap.data().PercentStore + "%");
                 $(".water").css({
                     "transform": "translateY(" + snap.data().WaveHeight + "%)"
                 });
-            });
 
-            var oldFlag = false;
-            db.collection("user").doc(user.uid).collection("Expense")
-                .get() //get collection of documents
-                .then(function (snap) {
-                    snap.forEach(function (doc) { //iterate thru collection
-                        var oldCategory = doc.data().Category;
-                        var oldDate = doc.data().Date;
-                        var oldValue = doc.data().Value;
-                        var oldNote = doc.data().Description;
-
-                        //Create new Time line Block
-                        var oldTimeLineBlock = $("<div></div>");
-
-                        if (!oldFlag) {
-                            oldFlag = true;
-                            $(oldTimeLineBlock).addClass("timeline-block timeline-block-left");
-                        } else {
-                            oldFlag = false;
-                            $(oldTimeLineBlock).addClass("timeline-block timeline-block-right");
-                        }
-
-                        var oldMarker = $("<div class=marker></div>");
-                        var oldTimeLineContent = $("<div class=timeline-content></div>");
-
-                        //Create new Timeline details
-                        var oldTimeLineTitle = $("<h3 class=category></h3>");
-                        oldTimeLineTitle.append(oldCategory, oldValue);
-
-                        var oldTimeLineDate = $("<span class=date></span>");
-                        oldTimeLineDate.append(oldDate);
-
-                        var oldTimeLineNote = $("<p class=description></p>");
-                        oldTimeLineNote.append(oldNote);
-
-                        oldTimeLineContent.append(oldTimeLineTitle, oldTimeLineDate, oldTimeLineNote);
-                        oldTimeLineBlock.append(oldMarker, oldTimeLineContent);
-
-                        $(".timeline-container").append(oldTimeLineBlock);
-
-
-                    });
+            } else {
+                $(".budget_percent_num").html(100 + "%");
+                $(".water").css({
+                    "transform": "translateY(" + snap.data().WaveHeight + "%)"
                 });
-
+            }
         });
 
-    }
-    createUser();
+        var oldFlag = false;
+        db.collection("user").doc(user.uid).collection("Expense").orderBy("Date", "asc")
+            .get() //get collection of documents
+
+            .then(function (snap) {
+                snap.forEach(function (doc) { //iterate thru collection
+                    var oldCategory = doc.data().Category;
+                    var oldDate = doc.data().Date;
+                    var oldValue = doc.data().Value;
+                    var oldNote = doc.data().Description;
+
+                    //Create new Time line Block
+                    var oldTimeLineBlock = $("<div></div>");
+
+                    if (!oldFlag) {
+                        oldFlag = true;
+                        $(oldTimeLineBlock).addClass("timeline-block timeline-block-left");
+                    } else {
+                        oldFlag = false;
+                        $(oldTimeLineBlock).addClass("timeline-block timeline-block-right");
+                    }
+
+                    var oldMarker = $("<div class=marker></div>");
+                    var oldTimeLineContent = $("<div class=timeline-content></div>");
+
+                    //Create new Timeline details
+                    var oldTimeLineTitle = $("<h3 class=category></h3>");
+                    oldTimeLineTitle.append(oldCategory, oldValue);
+
+                    var oldTimeLineDate = $("<span class=date></span>");
+                    oldTimeLineDate.append(oldDate);
+
+                    var oldTimeLineNote = $("<p class=description></p>");
+                    oldTimeLineNote.append(oldNote);
+
+                    oldTimeLineContent.append(oldTimeLineTitle, oldTimeLineDate, oldTimeLineNote);
+                    oldTimeLineBlock.append(oldMarker, oldTimeLineContent);
+
+                    $(".timeline-container").append(oldTimeLineBlock);
+                });
+            });
+    });
 
     //Create new expense function
     class Expense {
@@ -119,6 +137,7 @@ $(document).ready(function () {
             return this.newNote;
         }
     }
+
     //Create new expense item object
     let userExpense = new Expense();
 
@@ -135,17 +154,6 @@ $(document).ready(function () {
         "easing": "swing",
         "opacity": "0"
     };
-
-    //Expense Details Slide in: open category page
-    $("#create-icon").click(function () {
-        //if budget is 0 an d trying to create new expense: throw error
-        if (budgetStore == 0) {
-            $("#nobudget-modal").fadeIn();
-        } else {
-            $("#slideShow").animate(slideIn, 1000);
-            $("body").css("overflow", "hidden");
-        }
-    });
 
     //Open up the budget setup modal
     $("#home-icon").click(function () {
@@ -223,7 +231,6 @@ $(document).ready(function () {
     var expenseList = [];
 
     $("#save-btn").click(function () {
-
         //Retrive value from the input type
         userExpense.date = document.getElementById('datepicker').value;
         userExpense.amount = document.getElementById('amount-field').value;
@@ -231,22 +238,17 @@ $(document).ready(function () {
         expenseList.push(userExpense.amount);
 
         //Update expenses
-        var expenseTotal = 0;
+        if (expenseUpdate > 0) {
+            var expenseTotal = expenseUpdate;
+        } else {
+            expenseTotal = 0;
+        }
+        var expenseTotal = expenseUpdate;
         for (var i in expenseList) {
             expenseTotal += (1 * expenseList[i]);
+            expenseList.shift();
         }
-
-        //Write total expense to firebase
-        firebase.auth().onAuthStateChanged(function (user) {
-            db.collection("user")
-                .doc(user.uid)
-                .set({
-                    "ExpenseStore": parseInt(expenseTotal),
-                }, {
-                    merge: true
-                });
-        });
-
+        console.log(expenseList);
         //Write the new expenses
         if (expenseTotal > budgetStore) {
             //Open up the alert modal
@@ -294,7 +296,6 @@ $(document).ready(function () {
 
             var budgetController = budgetStore;
             budgetController -= expenseTotal;
-            // console.log("show expnese" + expenseList);
 
             var water = $(".water");
             var calPercent = updatePercent * (-1);
@@ -313,14 +314,15 @@ $(document).ready(function () {
                     .set({
                         "PercentStore": parseInt(updatePercent),
                         "WaveHeight": parseInt(waveHeight),
+                        "ExpenseStore": parseInt(expenseTotal),
                     }, {
                         merge: true
                     });
             });
 
             //Exit enter slide animation
+            $("#slideShow").animate(slideOut, 1000);
             $("#enter-slide").animate(infoSlideOut, 1000);
-            $("#slideShow").animate(slideOut, 500);
             $("body").css("overflow", "auto");
         }
 
@@ -342,18 +344,41 @@ $(document).ready(function () {
         writeExpenseEvent();
     });
 
-    //Set the budet number
-    var budgetStore = 0;
+    //Set the budget number
     $("#submit-budget-btn").click(function () {
         var userInput = document.getElementById('user-budget').value;
-
         if (userInput < 0) {
             alert('Value cannot be negative, change to modal later');
         } else {
             $("#budget_store").html("$" + (userInput / 1));
             $("#setup-budget").fadeOut();
             budgetStore = userInput;
+            console.log(budgetStore);
             firebase.auth().onAuthStateChanged(function (user) {
+                db.collection("user").doc(user.uid).onSnapshot(function (snap) {
+                    var expenseRefresh = snap.data().ExpenseStore;
+                    var budgetRefresh = snap.data().BudgetStore;
+                    //Refresh the percentage
+                    var newPercent = (1 - (expenseRefresh / budgetRefresh)) * 100;
+
+                    if (expenseRefresh > 0) {
+                        $(".budget_percent_num").html(newPercent.toFixed(1) + "%");
+                    }
+
+                    var newHeight = (newPercent * (-1)) + 88;
+                    $(".water").css({
+                        "transform": "translateY(" + newHeight + "%)"
+                    });
+
+                    db.collection("user").doc(user.uid).update({
+                        "PercentStore": parseInt(newPercent)
+                    });
+
+                    db.collection("user").doc(user.uid).update({
+                        "WaveHeight": parseInt(newHeight)
+                    });
+                });
+
                 db.collection("user")
                     .doc(user.uid)
                     .set({
@@ -361,9 +386,12 @@ $(document).ready(function () {
                     }, {
                         merge: true
                     });
+
             });
         }
+
     });
+
 
     //Direct to the setting budget function
     $("#setup-btn-error").click(function () {
@@ -380,7 +408,7 @@ $(document).ready(function () {
         $("#setup-budget").fadeOut();
     });
 
-    //Open the other menu widndow
+    //Open the other menu widndow: about us, contact page, our project
     var otherSlideIn = {
         "margin-top": "0",
         "easing": "swing",
@@ -392,6 +420,19 @@ $(document).ready(function () {
         "easing": "swing",
         "opacity": "0"
     };
+
+
+    //Expense Details Slide in: open category page
+    $("#create-icon").click(function () {
+
+        //if budget is 0 and trying to create new expense: throw error
+        if (budgetStore == 0) {
+            $("#nobudget-modal").fadeIn();
+        } else {
+            $("#slideShow").animate(slideIn, 1000);
+            $("body").css("overflow", "hidden");
+        }
+    });
 
     $("#other-icon").click(function () {
         $("#other-menu-modal").animate(otherSlideIn, 1000);
