1425a00f69d41b56737a564604e8a6d76c7f6688
lynnoj <jonnylu@live.ca>
2019-12-05 00:35:03 -0800

    Added a slider to the image, however it is not responsive yet and there is no buttons

---
+++ JS/OurProject.js
@@ -0,0 +1,41 @@
+'use strict';
+
+$(function () {
+
+    // Configuration
+    var width = 460;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    // cache DOM
+    var $slider = $("#slider");
+    var $slideContainer = $slider.find(".slides");
+    var $slides = $slideContainer.find(".slide");
+
+    var interval;
+
+    function startSlider() {
+        setInterval(function () {
+            $slideContainer.animate({"margin-left": "-=" + width}, animationSpeed, function () {
+                currentSlide++;
+                if (currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css("margin-left", 0);
+                }
+            });
+        }, pause);
+    }
+
+    function stopSlider() {
+        clearInterval(interval);
+    }
+
+    $slider.on("mouseenter", stopSlider).on("mouseleave", startSlider);
+
+    startSlider();
+
+    // listen for mouseenter and pause
+    // resume on mouseleave
+    
+});
